57d719950c3f1a872f49547e92fdd6266e0f31bd
fredy <fredy@fredy-Lenovo-B490>
2016-10-13 10:52:32 +0700

    alert widget

---
+++ views/alert.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @var yii\web\View $this
+ */
+$alertTypes = ['success', 'info', 'warning', 'danger', 'error' => 'danger'];
+
+foreach ($alertTypes as $name => $cssClass)
+{
+    if (is_integer($name))
+    {
+        $name = $cssClass;
+    }
+
+    $alert = Yii::$app->session->getFlash($name);
+
+    if (empty($alert))
+    {
+        continue;
+    }
+
+    if (is_scalar($alert))
+    {
+        $message = $alert;
+    }
+    elseif (is_array($alert))
+    {
+        $message = implode("\n<br/>\n", $alert);
+    }
+    else
+    {
+        $message = print_r($alert, true);
+    }
+
+    echo <<<HTML
+    <div class="alert alert-{$cssClass} alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {$message}
+    </div>\n
+HTML;
+}
